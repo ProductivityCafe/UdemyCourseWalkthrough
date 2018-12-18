914bf50911421f381413dd577af6c112b5a9d08d
Udemy Instructor <udemyinstructor@Mickeys-MacBook-Air.local>
2018-12-18 01:30:36 -0500

    nav-act-v5-done

---
+++ app/src/main/java/course/intermediate/notes/NavigationActivity.java
@@ -0,0 +1,41 @@
+package course.intermediate.notes;
+
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class NavigationActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_tasks:
+                    mTextMessage.setText(R.string.title_task);
+                    return true;
+                case R.id.navigation_notes:
+                    mTextMessage.setText(R.string.title_note);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigation);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
