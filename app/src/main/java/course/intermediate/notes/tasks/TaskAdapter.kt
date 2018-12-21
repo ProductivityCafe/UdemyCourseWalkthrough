ddf69a01a818744c6226917119ad5525437c1c72
Udemy Instructor <udemyinstructor@Mickeys-MacBook-Air.local>
2018-12-20 23:26:11 -0500

    added task recycler view

---
+++ app/src/main/java/course/intermediate/notes/tasks/TaskAdapter.kt
@@ -0,0 +1,34 @@
+package course.intermediate.notes.tasks
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import course.intermediate.notes.R
+import course.intermediate.notes.models.Task
+import kotlinx.android.synthetic.main.item_task.view.*
+
+class TaskAdapter(
+    private val taskList: MutableList<Task> = mutableListOf()
+): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+        ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false))
+
+
+    override fun getItemCount(): Int = taskList.size
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        (holder as ViewHolder).onBind(taskList[position])
+    }
+
+
+    class ViewHolder(val view: View): RecyclerView.ViewHolder(view) {
+
+        fun onBind(task: Task) {
+            view.titleView.text = task.title
+        }
+
+    }
+
+}
--- app/src/main/java/course/intermediate/notes/tasks/TasksListFragment.kt
+++ app/src/main/java/course/intermediate/notes/tasks/TasksListFragment.kt
@@ -6,8 +6,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import course.intermediate.notes.R
+import course.intermediate.notes.models.Task
+import kotlinx.android.synthetic.main.fragment_tasks_list.*
 
 class TasksListFragment : Fragment() {
 
@@ -21,6 +24,18 @@ class TasksListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_tasks_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        val adapter = TaskAdapter(mutableListOf(
+            Task("Testing One!"),
+            Task("Testing Two!")
+        ))
+        recyclerView.adapter = adapter
+
     }
 
     companion object {
